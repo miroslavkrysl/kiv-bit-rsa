3db7658a478b00bf95529c1352361725ad460fb9
Miroslav Krýsl <wilyxjam@gmail.com>
2019-03-31 13:22:05 +0200

    Add __init__.py into kiv_bit_rsa module

---
+++ kiv_bit_rsa/__init__.py
@@ -0,0 +1,11 @@
+"""KIV-BIT-RSA
+A module for the RSA encryption and decryption, MD5 hashing as well as making digital signatures
+using the RSA and MD5.
+"""
+
+from kiv_bit_rsa import math
+from kiv_bit_rsa import rsa
+
+__all__ = ["math", "rsa"]
+
+# TODO: add md5 and sign into the kiv_bit_rsa __init__.py
