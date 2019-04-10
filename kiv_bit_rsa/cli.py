c19d2594d80ce6bf09c46484cf6ccd75abd14548
Miroslav Krýsl <wilyxjam@gmail.com>
2019-04-10 18:53:18 +0200

    Implement keygen cli command

--- kiv_bit_rsa/cli.py
+++ kiv_bit_rsa/cli.py
@@ -6,49 +6,61 @@ signing and verifying
 
 import click
 
+from kiv_bit_rsa.rsa import generate_keys
+
 
 @click.group()
 def cli():
+    """RSA cipher utilities for encryption/decryption and signing/verifying files."""
     pass
 
 
-@click.command(help="Generate pair of RSA keys.")
+@click.command()
 @click.option('-b', '--bits', default=2048, type=click.IntRange(16, 8192), help='number of bits for storing key modulus n')
-@click.option('-d', '--private_key_file', 'private', default="rsa-key.public.toml", type=click.File('w'), help='filepath where to store private key')
-@click.option('-e', '--public_key_file', 'public', default=1, help='filepath where to store public key')
+@click.option('-d', '--private_key_file', 'private', default="rsa-key.private.toml", type=click.File('w'), help='filepath where to store private key')
+@click.option('-e', '--public_key_file', 'public', default="rsa-key.public.toml", type=click.File('w'), help='filepath where to store public key')
 def keygen(bits, private, public):
-    click.echo('Generating')
+    """Generate pair of RSA keys."""
+
+    keys = generate_keys(bits)
+
+    private.write(keys['private'].to_toml())
+    public.write(keys['public'].to_toml())
 
 
-@click.command(help="Encrypt a message using the RSA key.")
+@click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the encryption key')
 @click.option('-f', '--input', 'inp', default="-", type=click.File('r'), help='filepath where to read message from')
 @click.option('-o', '--output', 'out', default="-", type=click.File('w'), help='filepath where to print encrypted message into')
 def encrypt(key, input, output):
+    """Encrypt a message using the RSA key."""
     click.echo('Encrypting')
 
 
-@click.command(help="Decrypt a message using the RSA key.")
+@click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the decryption key')
 @click.option('-f', '--input', 'inp', default="-", type=click.File('r'), help='filepath where to read encrypted message from')
 @click.option('-o', '--output', 'out', default="-", type=click.File('w'), help='filepath where to print decrypted message into')
 def decrypt(key, inp, out):
+    """Decrypt a message using the RSA key."""
     click.echo('Decrypting')
 
 
-@click.command(help="Sign a file using the MD5 hash and RSA key.")
+@click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the signing key')
 @click.option('-f', '--file', 'file', required=True, type=click.File('r'), help='filepath of the file that will be signed')
 @click.option('-s', '--signature_file', 'sign', default="signature.toml", type=click.File('w'), help='filepath where to store the signature')
 def sign(key, file, sign):
+    """Sign a file using the MD5 hash and RSA key."""
     click.echo('Signing')
 
 
-@click.command(help="Verify a signed file.")
+@click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the decryption key')
 @click.option('-f', '--file', 'file', required=True, type=click.File('r'), help='filepath of the file that will be verified')
 @click.option('-s', '--signature_file', 'sign', default="signature.toml", type=click.File('r'), help='filepath of the signature')
 def verify(key, file, sign):
+    """Verify a signed file."""
     click.echo('Verifying')
 
 
