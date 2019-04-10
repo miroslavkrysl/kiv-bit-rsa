f216941346e36a3becff72e1cb7a6fa44e418a50
Miroslav Krýsl <wilyxjam@gmail.com>
2019-04-10 19:06:31 +0200

    Refactor math module

--- kiv_bit_rsa/math/__init__.py
+++ kiv_bit_rsa/math/__init__.py
@@ -3,6 +3,6 @@ A simple math module containing a couple of mathematical functions
 used by the RSA cipher.
 """
 
-from kiv_bit_rsa.math.math import get_random_prime, is_prime, modular_multiplicative_inverse
+from kiv_bit_rsa.math.math import random_prime, is_prime, mod_inverse
 
-__all__ = ["get_random_prime", "is_prime", "modular_multiplicative_inverse"]
+__all__ = ["random_prime", "is_prime", "mod_inverse"]
--- kiv_bit_rsa/math/math.py
+++ kiv_bit_rsa/math/math.py
@@ -1,39 +1,28 @@
-"""
-Useful mathematical functions for the RSA cipher.
+"""Useful mathematical functions for the RSA cipher.
 """
 
-from math import ceil
-from math import log
 from random import getrandbits
 from random import randrange
 
 
-def is_prime(num, certainty=0.99):
-    """
-    Test if num is prime number using the Miller-Rabin primarily test.
-    The certainty, that num is prime will be higher then given certainty.
+def is_prime(num, rounds=40):
+    """Test if `num` is prime number.
 
-    :param num: The integer number to be tested.
-    :param certainty: The minimal certainty - must between 0.0 and 1.0 exclusively.
-    :return: True if the num is prime, False otherwise.
-    """
+    Uses the Miller-Rabin probabilistic primality test.
+    The certainty of error is (1/4)^`rounds`.
 
-    if certainty <= 0.0 or certainty >= 1.0:
-        raise ValueError('certainty is out of range')
+    :param num: The number to be tested for primality.
+    :param rounds: Number of testing rounds.
+    :return: True if `num` is prime, False otherwise
+    """
 
     num = int(num)
 
-    if num <= 1:
-        return False
-
-    if num == 2:
+    if num == 1 or num == 2:
         return True
 
     if num % 2 == 0:
         return False
-
-    # compute minimum of required iterations to reach to desired certainty
-    k = - int(ceil(log(4, certainty)))
 
     r = 0
     s = num - 1
@@ -42,7 +31,7 @@ def is_prime(num, certainty=0.99):
         r += 1
         s //= 2
 
-    for _ in range(k):
+    for _ in range(rounds):
         a = randrange(2, num - 1)
         x = pow(a, s, num)
 
@@ -59,30 +48,38 @@ def is_prime(num, certainty=0.99):
     return True
 
 
-def get_random_prime(n_bits, prime_certainty=0.99):
-    """
-    Generate a prime number with maximal length of n_bits.
+def random_prime(n_bits):
+    """Generate a prime number with length of `n_bits`.
 
-    :param n_bits: The maximum number of number bits.
-    :param prime_certainty: The certainty of the number to by the prime
-    :return: The integer prime number.
+    :param n_bits: Number of bits ot the prime.
+    :return: Random prime.
     """
 
     while True:
+
         num = getrandbits(n_bits)
 
-        if is_prime(num, prime_certainty):
+        # make sure the number is odd
+        num |= 1
+
+        # make sure the number has exactly n_bits
+        num |= 2 ** (n_bits - 1)
+
+        if is_prime(num):
             return num
 
 
-def modular_multiplicative_inverse(a, n):
+def mod_inverse(a, n):
     """
-    Compute the modular multiplicative inverse of a modulo n.
+    Compute the modular multiplicative inverse of `a` modulo `n`.
 
     :param a: The integer to be inverted.
     :param n: The modulo n.
-    :return: The modular multiplicative inverse of a if exists, None otherwise.
+    :return: The modular multiplicative inverse of `a` if exists, None otherwise.
     """
+
+    if n == 0:
+        return None
 
     t = 0
     new_t = 1
