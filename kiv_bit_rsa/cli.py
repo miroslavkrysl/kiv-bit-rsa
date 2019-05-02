711b4a5a9f37de0088135b01f27b7f3cf2fc031f
Miroslav Krýsl <wilyxjam@gmail.com>
2019-05-02 14:01:24 +0200

    Prepare cli for signatures.

--- kiv_bit_rsa/cli.py
+++ kiv_bit_rsa/cli.py
@@ -6,7 +6,9 @@ signing and verifying
 
 import click
 
-from kiv_bit_rsa.rsa.key import Key
+from kiv_bit_rsa.hash import Md5
+from kiv_bit_rsa.rsa import Rsa, TomlKeyFormatter
+from kiv_bit_rsa.sign import SignableFile, Signature
 
 
 @click.group()
@@ -22,46 +24,47 @@ def cli():
 def keygen(bits, private, public):
     """Generate pair of RSA keys."""
 
-    from kiv_bit_rsa.rsa import generate_keys
+    rsa = Rsa()
+    keys = rsa.generate_keys(bits)
 
-    keys = generate_keys(bits)
+    formatter = TomlKeyFormatter()
 
-    private.write(keys['private'].to_toml())
-    public.write(keys['public'].to_toml())
+    private.write(formatter.to_string(keys.private_key))
+    public.write(formatter.to_string(keys.public_key))
 
 
 @click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the encryption key')
-@click.option('-f', '--input', 'inp', default="-", type=click.File('rb'), help='filepath where to read message from')
-@click.option('-o', '--output', 'out', default="-", type=click.File('wb'), help='filepath where to print encrypted message into')
-def encrypt(key, inp, out):
+@click.option('-p', '--plaintext', 'plaintext', default="-", type=click.File('rb'), help='filepath where to read plaintext from')
+@click.option('-c', '--cipher', 'cipher', default="-", type=click.File('wb'), help='filepath where to print the cipher into')
+def encrypt(key, plaintext, cipher):
     """Encrypt a message using the RSA key."""
 
-    from kiv_bit_rsa.rsa import encrypt
+    rsa = Rsa()
 
-    k = Key.from_toml(key.read())
+    k = TomlKeyFormatter().from_string(key.read())
 
-    p = inp.read()
-    c = encrypt(p, k)
+    p = plaintext.read()
+    c = rsa.encrypt(p, k)
 
-    out.write(c)
+    cipher.write(c)
 
 
 @click.command()
 @click.option('-k', '--key_file', 'key', required=True, type=click.File("r"), help='filepath of the decryption key')
-@click.option('-f', '--input', 'inp', default="-", type=click.File('rb'), help='filepath where to read encrypted message from')
-@click.option('-o', '--output', 'out', default="-", type=click.File('wb'), help='filepath where to print decrypted message into')
-def decrypt(key, inp, out):
+@click.option('-c', '--cipher', 'cipher', default="-", type=click.File('rb'), help='filepath where to read cipher from')
+@click.option('-p', '--plaintext', 'plaintext', default="-", type=click.File('wb'), help='filepath where to print plaintext into')
+def decrypt(key, cipher, plaintext):
     """Decrypt a message using the RSA key."""
 
-    from kiv_bit_rsa.rsa import decrypt
+    rsa = Rsa()
 
-    k = Key.from_toml(key.read())
+    k = TomlKeyFormatter().from_string(key.read())
 
-    c = inp.read()
-    p = decrypt(c, k)
+    c = cipher.read()
+    p = rsa.decrypt(c, k)
 
-    out.write(p)
+    plaintext.write(p)
 
 
 @click.command()
@@ -70,7 +73,11 @@ def decrypt(key, inp, out):
 @click.option('-s', '--signature_file', 'sign', default="signature.toml", type=click.File('w'), help='filepath where to store the signature')
 def sign(key, file, sign):
     """Sign a file using the MD5 hash and RSA key."""
-    click.echo('Signing')
+
+    key = TomlKeyFormatter().from_string(key.read())
+    signature = Signature.sign(SignableFile(file), Md5, key)
+
+    sign.write(TomlSignatureFormatter().to_string(signature))
 
 
 @click.command()
@@ -79,7 +86,16 @@ def sign(key, file, sign):
 @click.option('-s', '--signature_file', 'sign', default="signature.toml", type=click.File('r'), help='filepath of the signature')
 def verify(key, file, sign):
     """Verify a signed file."""
-    click.echo('Verifying')
+
+    key = TomlKeyFormatter().from_string(key.read())
+    signature = TomlSignatureFormatter().from_string(sign.read())
+
+    if signature.verify(SignableFile(file)):
+        click.echo("---verified---")
+        exit(0)
+    else:
+        click.echo("---denied---")
+        exit(1)
 
 
 cli.add_command(keygen)
